85cfb6e8fab444e3f78684462f0e391ec3792e2b
xMichaelSx <m.shapira5@gmail.com>
2021-07-31 20:23:29 +0300

    Fixed json parsing, deleted unnecessary User values and fixed other syntax problems, Fixed POST request

--- main.go
+++ main.go
@@ -16,15 +16,15 @@ const (
 	host     = "localhost"
 	port     = 5432
 	user     = "postgres"
-	password = "your_password"
+	password = "1948"
 	db_name  = "test"
 )
 
 type User struct {
-	UserID    string `json:"id"`
-	Name      string `json:"name"`
-	LastName  string `json:"lastname"`
-	Age       string `json:"age"`
+	UserID   string `json:"id"`
+	Name     string `json:"name"`
+	LastName string `json:"lastname"`
+	// Age       string `json:"age"`
 	BirthDate string `json:"birthdate"`
 }
 
@@ -37,11 +37,11 @@ type JsonResponse struct {
 func main() {
 
 	router := mux.NewRouter()
-
 	// Create a user
+
 	router.HandleFunc("/users", CreateUser).Methods("POST")
 
-	log.Fatal(http.ListenAndServe(":5432", router))
+	log.Fatal(http.ListenAndServe(":8080", router))
 
 }
 
@@ -50,22 +50,22 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Accept", "application/json")
 
-	var user User
-	_ = json.NewDecoder(r.Body).Decode(&user)
+	var My_user User
+	_ = json.NewDecoder(r.Body).Decode(&My_user)
 
-	user.UserID = r.Form.Get("id")
-	user.Name = r.Form.Get("name")
-	user.LastName = r.Form.Get("lastname")
-	user.Age = r.Form.Get("age")
-	user.BirthDate = r.Form.Get("birthdate")
+	// user.UserID = r.Form.Get("id")
+	// user.Name = r.Form.Get("name")
+	// user.LastName = r.Form.Get("lastname")
+	// // user.Age = r.Form.Get("age")
+	// user.BirthDate = r.Form.Get("birthdate")
 
 	var response = JsonResponse{}
 
-	if user.UserID == "" || user.Name == "" || user.LastName == "" || user.Age == "" || user.BirthDate == "" {
+	if My_user.UserID == "" || My_user.Name == "" || My_user.LastName == "" || My_user.BirthDate == "" {
 		response = JsonResponse{Type: "error", Message: "parameters are missing!"}
 	} else {
 
-		dbinfo := fmt.Sprintf("postgresql://host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, db_name)
+		dbinfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, db_name)
 		conn, err := pgx.Connect(context.Background(), dbinfo) // connect to psql
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
@@ -73,8 +73,8 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 		}
 		defer conn.Close(context.Background())
 
-		var lastInsertID int
-		err = conn.QueryRow(context.Background(), "INSERT INTO users(id, name, last, age, birthdate) VALUES($1, $2, $3, $4, $5) returning id;", user.UserID, user.Name, user.LastName, user.Age, user.BirthDate).Scan(&lastInsertID)
+		var lastInsertID string
+		err = conn.QueryRow(context.Background(), "INSERT INTO users(id, name, last, birthdate) VALUES($1, $2, $3, $4) returning id;", My_user.UserID, My_user.Name, My_user.LastName, My_user.BirthDate).Scan(&lastInsertID)
 		checkErr(err)
 
 		response = JsonResponse{Type: "success", Message: "The user has been inserted successfully!"}
