d97881a862c0f38bbb82227059fe90956944182c
xMichaelSx <m.shapira5@gmail.com>
2021-08-04 13:29:28 +0300

    made the code a little bit more efficient

--- main.go
+++ main.go
@@ -12,19 +12,19 @@ import (
 	"github.com/jackc/pgx/v4"
 )
 
+var conn *pgx.Conn // global variable connection so we wont make a new connection for each query. (good for small projects)
+
 const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "1948"
-	db_name  = "test"
+	host     = "localhost" // default
+	port     = 5432        // default
+	user     = "postgres"  // default
+	password = "Edit your password"
+	db_name  = "Edit your DataBase Name"
 )
 
 type User struct {
-	UserID   string `json:"id"`
-	Name     string `json:"name"`
-	LastName string `json:"lastname"`
-	// Age       string `json:"age"`
+	Name      string `json:"name"`
+	LastName  string `json:"lastname"`
 	BirthDate string `json:"birthdate"`
 }
 
@@ -37,49 +37,48 @@ type JsonResponse struct {
 func main() {
 
 	router := mux.NewRouter()
-	// Create a user
+	var err error
+	dbinfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, db_name)
+	conn, err = pgx.Connect(context.Background(), dbinfo) // connect to psql
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+	fmt.Println("listening on 8080 port")
 
 	router.HandleFunc("/users", CreateUser).Methods("POST")
+	router.HandleFunc("/users", GetUser).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 
 }
 
+func GetUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Starting...")
 	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Accept", "application/json")
+	// w.Header().Set("Accept", "application/json")
 
 	var My_user User
-	_ = json.NewDecoder(r.Body).Decode(&My_user)
-
-	// user.UserID = r.Form.Get("id")
-	// user.Name = r.Form.Get("name")
-	// user.LastName = r.Form.Get("lastname")
-	// // user.Age = r.Form.Get("age")
-	// user.BirthDate = r.Form.Get("birthdate")
+	err := json.NewDecoder(r.Body).Decode(&My_user)
+	checkErr(err)
 
 	var response = JsonResponse{}
 
-	if My_user.UserID == "" || My_user.Name == "" || My_user.LastName == "" || My_user.BirthDate == "" {
-		response = JsonResponse{Type: "error", Message: "parameters are missing!"}
-	} else {
-
-		dbinfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, db_name)
-		conn, err := pgx.Connect(context.Background(), dbinfo) // connect to psql
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-			os.Exit(1)
-		}
-		defer conn.Close(context.Background())
-
-		var lastInsertID string
-		err = conn.QueryRow(context.Background(), "INSERT INTO users(id, name, last, birthdate) VALUES($1, $2, $3, $4) returning id;", My_user.UserID, My_user.Name, My_user.LastName, My_user.BirthDate).Scan(&lastInsertID)
-		checkErr(err)
-
-		response = JsonResponse{Type: "success", Message: "The user has been inserted successfully!"}
-
+	if My_user.Name == "" || My_user.LastName == "" || My_user.BirthDate == "" {
+		response = JsonResponse{Type: "error", Data: []User{My_user}, Message: "parameters are missing from Post request!"}
+		json.NewEncoder(w).Encode(response)
+		return
 	}
+
+	var lastInsertID int
+	err = conn.QueryRow(context.Background(), "INSERT INTO users(name, lastname, birthdate) VALUES($1, $2, $3) returning id;", My_user.Name, My_user.LastName, My_user.BirthDate).Scan(&lastInsertID)
+	checkErr(err)
+
+	response = JsonResponse{Type: "success", Data: []User{My_user}, Message: "The user has been inserted successfully!"}
 
 	json.NewEncoder(w).Encode(response)
 }
